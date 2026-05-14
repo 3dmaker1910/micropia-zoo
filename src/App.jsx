ec6d001e5d8d5fe8def77e7dfbd1d360401b88ff
3dmaker1910 <asnmaker@gmail.com>
2026-05-13 19:58:41 -0500

    chore: removed Biotecnofilos island and added AutofagiaRoom route

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,10 @@ import HantavirusMission from './components/HantavirusMission';
 import VectorRoom from './components/VectorRoom';
 import BodyScanner from './components/BodyScanner';
 import CyanobacteriaRoom from './components/CyanobacteriaRoom';
-import BiotecnofilosRoom from './components/BiotecnofilosRoom';
+import AutofagiaRoom from './components/AutofagiaRoom'; // New component to create
 
 function App() {
   const [screen, setScreen] = useState('landing');
-
-  const handleEnter = useCallback(() => {
-    setScreen('hall');
-  }, []);
-
-  const handleGoToHub = useCallback(() => {
-    setScreen('hub');
-  }, []);
-
-  const handleBackToHall = useCallback(() => {
-    setScreen('hall');
-  }, []);
-
-  const handleStartQuiz = useCallback(() => {
-    setScreen('quiz');
-  }, []);
-
-  const handleBackToHub = useCallback(() => {
-    setScreen('hub');
-  }, []);
-
-  const handleGoToMap = useCallback(() => {
-    setScreen('map');
-  }, []);
-
-  const handleGoToVectors = useCallback(() => {
-    setScreen('vectors');
-  }, []);
 
   const handleNavigate = useCallback((target) => {
     setScreen(target);
@@ -49,42 +21,16 @@ function App() {
   return (
     <div className="scanlines">
       <AnimatePresence mode="wait">
-        {screen === 'landing' && (
-          <LandingScreen key="landing" onEnter={handleEnter} />
-        )}
-        {screen === 'hall' && (
-          <ContainmentHall
-            key="hall"
-            onGoToHub={handleGoToHub}
-            onGoToMap={handleGoToMap}
-            onGoToVectors={handleGoToVectors}
-            onNavigate={handleNavigate}
-          />
-        )}
-        {screen === 'hub' && (
-          <DoctorHub key="hub" onBack={handleBackToHall} onStartQuiz={handleStartQuiz} />
-        )}
-        {screen === 'quiz' && (
-          <QuizSystem key="quiz" onBack={handleBackToHub} />
-        )}
-        {screen === 'map' && (
-          <GlobalIntelligenceMap key="map" onNavigate={handleNavigate} />
-        )}
-        {screen === 'mission' && (
-          <HantavirusMission key="mission" onNavigate={handleNavigate} />
-        )}
-        {screen === 'vectors' && (
-          <VectorRoom key="vectors" onNavigate={handleNavigate} />
-        )}
-        {screen === 'bodyscan' && (
-          <BodyScanner key="bodyscan" onNavigate={handleNavigate} />
-        )}
-        {screen === 'cyanobacteria' && (
-          <CyanobacteriaRoom key="cyanobacteria" onNavigate={handleNavigate} />
-        )}
-        {screen === 'biotecnofilos' && (
-          <BiotecnofilosRoom key="biotecnofilos" onNavigate={handleNavigate} />
-        )}
+        {screen === 'landing' && <LandingScreen key="landing" onEnter={() => setScreen('hall')} />}
+        {screen === 'hall' && <ContainmentHall key="hall" onGoToHub={() => setScreen('hub')} onGoToMap={() => setScreen('map')} onNavigate={handleNavigate} />}
+        {screen === 'hub' && <DoctorHub key="hub" onBack={() => setScreen('hall')} onStartQuiz={() => setScreen('quiz')} />}
+        {screen === 'quiz' && <QuizSystem key="quiz" onBack={() => setScreen('hub')} />}
+        {screen === 'map' && <GlobalIntelligenceMap key="map" onNavigate={handleNavigate} />}
+        {screen === 'mission' && <HantavirusMission key="mission" onNavigate={handleNavigate} />}
+        {screen === 'vectors' && <VectorRoom key="vectors" onNavigate={handleNavigate} />}
+        {screen === 'bodyscan' && <BodyScanner key="bodyscan" onNavigate={handleNavigate} />}
+        {screen === 'cyanobacteria' && <CyanobacteriaRoom key="cyanobacteria" onNavigate={handleNavigate} />}
+        {screen === 'autofagia' && <AutofagiaRoom key="autofagia" onNavigate={handleNavigate} />}
       </AnimatePresence>
     </div>
   );
